dd368bccdaf6ceced6742c007a4f279a4869bd04
Quackers <robq71@gmail.com>
2023-01-06 21:20:31 +0000

    tested API using post Method

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi import Body
 
 app = FastAPI()
 
@@ -16,3 +17,8 @@ async def root():
 @app.get("/posts")
 def get_posts():
     return {"data": "This is your posts"}
+
+@app.post("/createposts")
+def create_posts(payload: dict = Body(...)):
+    print(payload)
+    return {"new_post": f"title: {payload['title']} content: {payload['content']}"}
